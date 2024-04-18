dd941d8f135b684dc40a2ed2d77b5b111a06fb93
peng <peng@pengdeMacBook-Air.local>
2024-04-18 14:29:24 +0800

    write resolver code

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -17,6 +17,7 @@ func (r *mutationResolver) UpsertCharacter(ctx context.Context, input model.Char
 	id := input.ID
 	var character model.Character
 	character.Name = input.Name
+	character.CliqueType = input.CliqueType
 
 	n := len(r.Resolver.CharacterStore)
 	if n == 0 {
@@ -24,15 +25,23 @@ func (r *mutationResolver) UpsertCharacter(ctx context.Context, input model.Char
 	}
 
 	if id != nil {
-		_, ok := r.Resolver.CharacterStore[*id]
+		cs, ok := r.Resolver.CharacterStore[*id]
 		if !ok {
 			return nil, fmt.Errorf("not found")
+		}
+		if input.IsHero != nil {
+			character.IsHero = *input.IsHero
+		} else {
+			character.IsHero = cs.IsHero
 		}
 		r.Resolver.CharacterStore[*id] = character
 	} else {
 		// generate unique id
 		nid := strconv.Itoa(n + 1)
 		character.ID = nid
+		if input.IsHero != nil {
+			character.IsHero = *input.IsHero
+		}
 		r.Resolver.CharacterStore[nid] = character
 	}
 
@@ -49,14 +58,19 @@ func (r *queryResolver) Character(ctx context.Context, id string) (*model.Charac
 	return &character, nil
 }
 
-// Pogues is the resolver for the pogues field.
-func (r *queryResolver) Pogues(ctx context.Context) ([]*model.Character, error) {
-	panic(fmt.Errorf("not implemented: Pogues - pogues"))
-}
+// Characters is the resolver for the characters field.
+func (r *queryResolver) Characters(ctx context.Context, cliqueType model.CliqueType) ([]*model.Character, error) {
+	//panic(fmt.Errorf("not implemented: Characters - characters"))
+	characters := make([]*model.Character, 0)
+	for idx := range r.Resolver.CharacterStore {
+		character := r.Resolver.CharacterStore[idx]
+		if character.CliqueType == cliqueType {
 
-// Kooks is the resolver for the kooks field.
-func (r *queryResolver) Kooks(ctx context.Context) ([]*model.Character, error) {
-	panic(fmt.Errorf("not implemented: Kooks - kooks"))
+			characters = append(characters, &character)
+		}
+	}
+
+	return characters, nil
 }
 
 // Mutation returns MutationResolver implementation.
